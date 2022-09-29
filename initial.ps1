71357e872b6c5df20e40defab8131cd8c87718d1
neilrtaylor <43854119+neilrtaylor@users.noreply.github.com>
2022-09-29 13:25:51 +0100

    Update initial.ps1

--- initial.ps1
+++ initial.ps1
@@ -7,7 +7,8 @@ Set-ExecutionPolicy Unrestricted -Force
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
 
 # Create a Boxstarter package from a file in Github
-New-PackageFromScript -Source https://raw.githubusercontent.com/neilrtaylor/nrt01/main/install_sw_01 -PackageName install_sw_01
+#New-PackageFromScript -Source https://raw.githubusercontent.com/neilrtaylor/nrt01/main/install_sw_01 -PackageName install_sw_01
 
 # Install the package
-Install-BoxstarterPackage -PackageName install_sw_01
+#Install-BoxstarterPackage -PackageName install_sw_01
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/neilrtaylor/nrt01/main/package01
